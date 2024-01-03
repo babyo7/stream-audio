a4f7be73310ae5bc7b1428382e4d0d95adc31fe4
Aman <119067199+babyo7@users.noreply.github.com>
2024-01-04 03:50:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,28 +6,28 @@ const cors = require("cors");
 const fs = require("fs");
 
 app.use(cors());
-app.use(express.static('./'))
+app.use(express.static("./"));
 
 app.get("/", async (req, res) => {
-    try {
-        const link = req.query.url;
-        if (link) {
-          const id = StreamAudio.getVideoID(link);
-         if(fs.existsSync(`./music/${id}.mp4`)){
-             res.send(`/music/${id}.mp4`)
-             return
-         } 
-          const url = StreamAudio(link,{filter:'videoandaudio',quality:'highestvideo'}).pipe(fs.createWriteStream(`./music/${id}.mp4`))
-          url.on('finish',()=>{
-              res.send(`/music/${id}.mp4`);
-          })
-        } else {
-          res.json({ message: "url not provided" });
-        }
-    } catch (error) {
-        res.json({error: error.message});
+  try {
+    const link = req.query.url;
+    if (link) {
+      const id = StreamAudio.getVideoID(link);
+      const url = StreamAudio(link, {
+        filter: "videoandaudio",
+        quality: "highestvideo",
+      });
+      res.setHeader("content-type", "audio/mpeg");
+      url.on("error",(error)=>{
+        res.status(404).end()
+      })
+      url.pipe(res);
+    } else {
+      res.json({ message: "url not provided" });
     }
-
+  } catch (error) {
+    res.json({ error: error.message });
+  }
 });
 
 app.listen(port, () => {
