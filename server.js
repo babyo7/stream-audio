5c11838600d736bb1ba6212d6be2ac2779c3f708
Aman <119067199+babyo7@users.noreply.github.com>
2024-01-07 07:53:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const StreamAudio = require("ytdl-core");
+const ytdl = require("ytdl-core");
 const app = express();
 const port = process.env.PORT || 4000;
 const cors = require("cors");
@@ -14,35 +14,45 @@ app.get("/", async (req, res) => {
       return res.status(500).json({ message: "url not provided" });
     }
 
-    const info = await StreamAudio.getInfo(link);
+    const info = await ytdl.getInfo(link);
+    const formats = ytdl.filterFormats(info.formats, "videoandaudio");
+ 
+    if (formats.length === 0) {
+      return res.status(500).json({ message: "No audio format found" });
+    }
 
-    const length = info.formats[0].contentLength;
+    const audioFormat = formats[0];
+    const length = audioFormat.approxDurationMs;
+    console.log(length);
 
-    const url = StreamAudio(link, {
-      filter: "videoandaudio",
-      quality: "highestvideo",
-    });
-
-    res.setHeader("content-type", "audio/mpeg");
+    res.setHeader("Content-Type", "audio/mpeg");
     res.setHeader("Accept-Ranges", "bytes");
     res.setHeader("Content-Length", length);
+    res.setHeader("Content-Range", `bytes 0-${length - 1}/${length}`);
+    res.status(200);
 
-    url.on("error", (error) => {
-      console.log("Error Sending Stream");
-      url.destroy()
-      res.end();
+    const stream = ytdl(link, {
+      quality: "highestvideo",
+      filter: "videoandaudio",
+      highWaterMark: 1 << 25, 
     });
 
-    url.pipe(res);
+    stream.on("error", (error) => {
+      console.error(error);
+      res.status(404)
+      stream.destroy()
+      res.end();
+      return
+    });
+
+    stream.pipe(res);
   } catch (error) {
     res.status(500).json({ Error: error.message });
   }
 });
 
-
-
 app.listen(port, () => {
-  console.log(`http://localhost:${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
 
 module.exports = app;
