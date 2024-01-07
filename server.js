9d995afd2c0e3aa15afc1f0a9a6cc4996b919806
Aman <119067199+babyo7@users.noreply.github.com>
2024-01-07 15:47:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,47 +3,56 @@ const StreamAudio = require("ytdl-core");
 const app = express();
 const port = process.env.PORT || 4000;
 const cors = require("cors");
-const fs = require('fs')
+const fs = require("fs");
 
 app.use(cors());
 app.use(express.static("./"));
 
 app.get("/", async (req, res) => {
   try {
-    const link = req.query.url;
-    if (!link) {
-      return res.status(500).json({ message: "url not provided" });
+    const Link = req.query.url;
+    if (!Link) {
+      res.status(400).json({ error: "url not provided" });
+      return;
     }
+    const SongId = StreamAudio.getVideoID(Link);
 
-    const info = await StreamAudio.getInfo(link);
-
-    const length = info.formats[0].contentLength;
-
-    const url = StreamAudio(link, {
-      filter: "videoandaudio",
-      quality: "highestvideo",
-    });
-
-    res.setHeader("content-type", "audio/mpeg");
-    res.setHeader("Accept-Ranges", "bytes");
-  
-    url.on("error", (error) => {
-      console.log(error);
-    
-    });
-
-    url.pipe(res);
+    if (fs.existsSync(`music/${SongId}.mp4`)) {
+      SendStream(res, SongId);
+    } else {
+      const Download = StreamAudio(Link, {
+        filter: "videoandaudio",
+        quality: "highestvideo",
+      }).pipe(fs.createWriteStream(`music/${SongId}.mp4`));
+      Download.on("finish", () => {
+        SendStream(res, SongId);
+      });
+    }
   } catch (error) {
     res.status(500).json({ Error: error.message });
   }
 });
 
-
-
+function SendStream(res, Id) {
+  try {
+    const Data = fs.statSync(`music/${Id}.mp4`);
+    const file = fs.createReadStream(`music/${Id}.mp4`);
+  
+    res.setHeader("content-type", "audio/mp3");
+    res.setHeader("content-length", Data.size);
+     
+    console.log("streaming");
+     
+    file.pipe(res);
+    
+  } catch (error) {
+    console.log(error);
+  }
+ 
+}
 
 app.listen(port, () => {
   console.log(`http://localhost:${port}`);
 });
 
 module.exports = app;
-
