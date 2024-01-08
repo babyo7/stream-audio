dad47eca6f559fe887f7816fbd0a7b56c05eba7b
Aman <119067199+babyo7@users.noreply.github.com>
2024-01-09 01:22:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,59 +9,45 @@ app.use(cors());
 app.use(express.static("./"));
 
 app.get("/", async (req, res) => {
-  try {
-    const Link = req.query.url;
-    if (!Link) {
-      res.status(400).json({ error: "url not provided" });
-      return;
-    }
-    const SongId = StreamAudio.getVideoID(Link);
-
-    if (fs.existsSync(`music/${SongId}.mp3`)) {
-      const Data = fs.statSync(`music/${SongId}.mp3`);
-      if(Data.size == 0){
-        Download(Link,res)
+  const Link = req.query.url;
+  if (Link) {
+    try {
+      console.log(Link);
+      if (fs.existsSync(`music/${Link}.mp3`)) {
+        const Data = fs.statSync(`music/${Link}.mp3`);
+        const audio = fs.createReadStream(`music/${Link}.mp3`);
+        res.setHeader("content-type", "audio/mp3");
+        res.setHeader("content-length", Data.size);
+        console.log("piped");
+        audio.pipe(res);
+        return;
       }
-      SendStream(res, SongId);
-    } else {
-    Download(Link)
-    res.send("ok")
-    }
-  } catch (error) {
-    res.status(500).json({ Error: error.message });
+
+      const Download = StreamAudio(Link, {
+        filter: "videoandaudio",
+        quality: "highestvideo",
+      });
+
+      Download.on("error", (e) => {
+        res.status(404).json(e.message);
+        return;
+      });
+
+      const audio = Download.pipe(fs.createWriteStream(`music/${Link}.mp3`));
+
+      audio.on("finish", () => {
+        const Data = fs.statSync(`music/${Link}.mp3`);
+        const audio = fs.createReadStream(`music/${Link}.mp3`);
+        res.setHeader("content-type", "audio/mp3");
+        res.setHeader("content-length", Data.size);
+        console.log("piped");
+        audio.pipe(res);
+      });
+    } catch (error) {}
+  } else {
+    res.status(404).json("url not provided");
   }
 });
-
-function SendStream(res, Id) {
-  try {
-    const Data = fs.statSync(`music/${Id}.mp3`);
-    const file = fs.createReadStream(`music/${Id}.mp3`);
-  
-    res.setHeader("content-type", "audio/mp3");
-    res.setHeader("content-length", Data.size);
-  
-    console.log("streaming",Data.size);
-     
-    file.pipe(res);
-    
-  } catch (error) {
-    console.log(error);
-  }
- 
-}
-
-function Download(Link,res){
-  const SongId = StreamAudio.getVideoID(Link);
-  let Download = StreamAudio(Link, {
-    filter: "videoandaudio",
-    quality: "highestvideo",
-  }).pipe(fs.createWriteStream(`music/${SongId}.mp3`));
-
-  Download.on("finish", () => {
-    console.log(`music/${SongId}.mp3`)
-    SendStream(SongId, res);
-  });
-}
 
 app.listen(port, () => {
   console.log(`http://localhost:${port}`);
