bf8736434da4aaef3ab91689dc75a9d57fce8445
Aman <119067199+babyo7@users.noreply.github.com>
2024-01-09 02:13:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,33 +12,25 @@ app.get("/", async (req, res) => {
   const Link = req.query.url;
   if (Link) {
     try {
+      const id = StreamAudio.getVideoID(`https://www.youtube.com/watch?v=${Link}`)
       console.log(Link);
-      if (fs.existsSync(`music/${Link}.mp3`) > 0 ) {
-        const Data = fs.statSync(`music/${Link}.mp3`);
-        const audio = fs.createReadStream(`music/${Link}.mp3`);
-        res.setHeader("content-type", "audio/mp3");
-        res.setHeader("content-length", Data.size);
-        console.log("piped");
-        audio.pipe(res);
+      if (fs.existsSync(`music/${Link}.mp3`)) {
+        res.send(`/music/${Link}.mp3`)
         return;
       }
 
       const Download = StreamAudio(Link, {
         filter: "videoandaudio",
         quality: "highestvideo",
+      }).pipe(fs.createWriteStream(`music/${Link}.mp3`));
+
+      Download.on("finish", () => {
+       res.send(`/music/${Link}.mp3`)
       });
 
-      Download.on("error", (e) => {
-        res.status(404).json(e.message);
-        return;
-      });
-
-      const audio = Download.pipe(fs.createWriteStream(`music/${Link}.mp3`));
-
-      audio.on("finish", () => {
-      res.send('sex')
-      });
-    } catch (error) {}
+    } catch (error) {
+      res.json(error.message)
+    }
   } else {
     res.status(404).json("url not provided");
   }
