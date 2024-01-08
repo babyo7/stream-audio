c46e26f4d6814fc48024299fb1cf08c88001dc40
Aman <119067199+babyo7@users.noreply.github.com>
2024-01-08 23:15:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,9 +18,14 @@ app.get("/", async (req, res) => {
     const SongId = StreamAudio.getVideoID(Link);
 
     if (fs.existsSync(`music/${SongId}.mp3`)) {
+      const Data = fs.statSync(`music/${Id}.mp3`);
+      if(Data.size == 0){
+        Download(Link,res)
+      }
       SendStream(res, SongId);
     } else {
     Download(Link)
+    res.send("ok")
     }
   } catch (error) {
     res.status(500).json({ Error: error.message });
@@ -45,14 +50,16 @@ function SendStream(res, Id) {
  
 }
 
-function Download(Link){
+function Download(Link,res){
   const SongId = StreamAudio.getVideoID(Link);
   let Download = StreamAudio(Link, {
     filter: "videoandaudio",
     quality: "highestvideo",
   }).pipe(fs.createWriteStream(`music/${SongId}.mp3`));
+
   Download.on("finish", () => {
     console.log(`music/${SongId}.mp3`)
+    SendStream(SongId, res);
   });
 }
 
