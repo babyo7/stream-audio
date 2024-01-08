9c1b5b877f942614d0ed5b16a07b5ac7902b45a6
Aman <119067199+babyo7@users.noreply.github.com>
2024-01-08 13:28:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,13 +17,13 @@ app.get("/", async (req, res) => {
     }
     const SongId = StreamAudio.getVideoID(Link);
 
-    if (fs.existsSync(`music/${SongId}.mp4`)) {
+    if (fs.existsSync(`music/${SongId}.mp3`)) {
       SendStream(res, SongId);
     } else {
       const Download = StreamAudio(Link, {
         filter: "videoandaudio",
         quality: "highestvideo",
-      }).pipe(fs.createWriteStream(`music/${SongId}.mp4`));
+      }).pipe(fs.createWriteStream(`music/${SongId}.mp3`));
       Download.on("finish", () => {
         SendStream(res, SongId);
       });
@@ -35,10 +35,10 @@ app.get("/", async (req, res) => {
 
 function SendStream(res, Id) {
   try {
-    const Data = fs.statSync(`music/${Id}.mp4`);
-    const file = fs.createReadStream(`music/${Id}.mp4`);
+    const Data = fs.statSync(`music/${Id}.mp3`);
+    const file = fs.createReadStream(`music/${Id}.mp3`);
   
-    res.setHeader("content-type", "audio/mp3");
+    res.setHeader("content-type", "audio/mpeg");
     res.setHeader("content-length", Data.size);
      
     console.log("streaming");
