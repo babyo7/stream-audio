b6e6c6e32d9521b045c59da284d28e8d59ade8c0
Aman <119067199+babyo7@users.noreply.github.com>
2024-03-23 21:51:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -121,6 +121,76 @@
 // }
 
 
+// const express = require("express");
+// const StreamAudio = require("ytdl-core");
+// const app = express();
+// const port = process.env.PORT || 4000;
+// const cors = require("cors");
+// const fs = require("fs");
+// const cluster = require("node:cluster");
+// const os = require("os");
+// const totalCpu = os.cpus().length;
+
+// if (cluster.isPrimary) {
+//   for (let i = 0; i < totalCpu; i++) {
+//     cluster.fork();
+//   }
+// } else {
+//   app.use(cors());
+//   app.use(express.static("./"));
+
+//   app.get("/", async (req, res) => {
+//     const Link = req.query.url;
+//     const file = req.query.file;
+//     if (Link) {
+//       try {
+//         if (fs.existsSync(`music/${Link}.mp3`)) {
+//           const audio = fs.createReadStream(`music/${Link}.mp3`);
+//           const data = fs.statSync(`music/${Link}.mp3`);
+//           res.setHeader("content-type", "audio/mpeg");
+//           res.setHeader("content-length", data.size);
+//           res.setHeader(
+//             "Content-Disposition",
+//             `attachment; filename=${file}.mp3`
+//           );
+//           audio.pipe(res);
+//           return;
+//         }
+
+//         const Download = StreamAudio(Link, {
+//           filter: "videoandaudio",
+//           quality: "highestvideo",
+//         }).pipe(fs.createWriteStream(`music/${Link}.mp3`));
+
+//         Download.on("error", () => console.error("error"));
+//         Download.on("finish", () => {
+//           console.log(Link);
+//           const audio = fs.createReadStream(`music/${Link}.mp3`);
+//           const data = fs.statSync(`music/${Link}.mp3`);
+//           res.setHeader("content-type", "audio/mpeg");
+//           res.setHeader("content-length", data.size);
+//           res.setHeader(
+//             "Content-Disposition",
+//             `attachment; filename=${file}.mp3`
+//           );
+//           audio.pipe(res);
+//         });
+//       } catch (error) {
+//         console.log("error");
+//         res.json("error");
+//       }
+//     } else {
+//       res.status(200).json("url not provided");
+//     }
+//   });
+
+//   app.listen(port, () => {
+//     console.log(`http://localhost:${port}`);
+//   });
+// }
+
+
+
 const express = require("express");
 const StreamAudio = require("ytdl-core");
 const app = express();
@@ -140,6 +210,42 @@ if (cluster.isPrimary) {
   app.use(express.static("./"));
 
   app.get("/", async (req, res) => {
+    const Link = req.query.url;
+    const file = req.query.file;
+    if (Link) {
+      try {
+        if (fs.existsSync(`music/${Link}.mp3`)) {
+          const audio = fs.createReadStream(`music/${Link}.mp3`);
+          const data = fs.statSync(`music/${Link}.mp3`);
+          res.setHeader("content-type", "audio/mpeg");
+          res.setHeader("content-length", data.size);
+          audio.pipe(res);
+          return;
+        }
+
+        const Download = StreamAudio(Link, {
+          filter: "videoandaudio",
+          quality: "highestvideo",
+        }).pipe(fs.createWriteStream(`music/${Link}.mp3`));
+
+        Download.on("error", () => console.error("error"));
+        Download.on("finish", () => {
+          console.log(Link);
+          const audio = fs.createReadStream(`music/${Link}.mp3`);
+          const data = fs.statSync(`music/${Link}.mp3`);
+          res.setHeader("content-type", "audio/mpeg");
+          res.setHeader("content-length", data.size);
+          audio.pipe(res);
+        });
+      } catch (error) {
+        console.log("error");
+        res.json("error");
+      }
+    } else {
+      res.status(200).json("url not provided");
+    }
+  });
+  app.get("/download/", async (req, res) => {
     const Link = req.query.url;
     const file = req.query.file;
     if (Link) {
@@ -188,5 +294,6 @@ if (cluster.isPrimary) {
     console.log(`http://localhost:${port}`);
   });
 }
+
 
 
