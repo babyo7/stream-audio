2808524962395e36e682d57ad2b61031d1efcf50
Aman <119067199+babyo7@users.noreply.github.com>
2024-03-25 11:25:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -190,6 +190,116 @@
 // }
 
 
+// const express = require("express");
+// const StreamAudio = require("ytdl-core");
+// const app = express();
+// const port = process.env.PORT || 4000;
+// const cors = require("cors");
+// const fs = require("fs");
+// const cluster = require("node:cluster");
+// const os = require("os");
+// const totalCpu = os.cpus().length;
+
+// if (cluster.isPrimary) {
+//   for (let i = 0; i < totalCpu; i++) {
+//     cluster.fork();
+//   }
+// } else {
+//   app.use(cors());
+//   app.use(express.static("./"));
+
+//   app.get("/", async (req, res) => {
+//     const Link = req.query.url;
+//     if (Link) {
+//       try {
+//         if (fs.existsSync(`music/${Link}.mp3`)) {
+//           const audio = fs.createReadStream(`music/${Link}.mp3`);
+//           const data = fs.statSync(`music/${Link}.mp3`);
+//           res.setHeader("content-type", "audio/mpeg");
+//           res.setHeader("Accept-Ranges", "bytes");
+
+//           res.setHeader("content-length", data.size);
+//           audio.pipe(res);
+//           return;
+//         }
+
+//         const Download = StreamAudio(Link, {
+//           filter: "videoandaudio",
+//           quality: "highestvideo",
+//         }).pipe(fs.createWriteStream(`music/${Link}.mp3`));
+
+//         Download.on("error", () => console.error("error"));
+//         Download.on("finish", () => {
+//           console.log(Link);
+//           const audio = fs.createReadStream(`music/${Link}.mp3`);
+//           const data = fs.statSync(`music/${Link}.mp3`);
+//           res.setHeader("content-type", "audio/mpeg");
+//           res.setHeader("Accept-Ranges", "bytes");
+//           res.setHeader("content-length", data.size);
+//           audio.pipe(res);
+//         });
+//       } catch (error) {
+//         console.log("error");
+//         res.json("error");
+//       }
+//     } else {
+//       res.status(200).json("url not provided");
+//     }
+//   });
+//   app.get("/download/", async (req, res) => {
+//     const Link = req.query.url;
+//     const file = req.query.file;
+//     if (Link) {
+//       try {
+//         if (fs.existsSync(`music/${Link}.mp3`)) {
+//           const audio = fs.createReadStream(`music/${Link}.mp3`);
+//           const data = fs.statSync(`music/${Link}.mp3`);
+//           res.setHeader("content-type", "audio/mpeg");
+//           res.setHeader("content-length", data.size);
+//           res.setHeader("Accept-Ranges", "bytes");
+//           res.setHeader(
+//             "Content-Disposition",
+//             `attachment; filename=${file}.mp3`
+//           );
+//           audio.pipe(res);
+//           return;
+//         }
+
+//         const Download = StreamAudio(Link, {
+//           filter: "videoandaudio",
+//           quality: "highestvideo",
+//         }).pipe(fs.createWriteStream(`music/${Link}.mp3`));
+
+//         Download.on("error", () => console.error("error"));
+//         Download.on("finish", () => {
+//           console.log(Link);
+//           const audio = fs.createReadStream(`music/${Link}.mp3`);
+//           const data = fs.statSync(`music/${Link}.mp3`);
+//           res.setHeader("content-type", "audio/mpeg");
+//           res.setHeader("content-length", data.size);
+//           res.setHeader("Accept-Ranges", "bytes");
+//           res.setHeader(
+//             "Content-Disposition",
+//             `attachment; filename=${file}.mp3`
+//           );
+//           audio.pipe(res);
+//         });
+//       } catch (error) {
+//         console.log("error");
+//         res.json("error");
+//       }
+//     } else {
+//       res.status(200).json("url not provided");
+//     }
+//   });
+
+//   app.listen(port, () => {
+//     console.log(`http://localhost:${port}`);
+//   });
+// }
+
+
+
 const express = require("express");
 const StreamAudio = require("ytdl-core");
 const app = express();
@@ -199,6 +309,8 @@ const fs = require("fs");
 const cluster = require("node:cluster");
 const os = require("os");
 const totalCpu = os.cpus().length;
+const cp = require("child_process");
+const ffmpeg = require("ffmpeg-static");
 
 if (cluster.isPrimary) {
   for (let i = 0; i < totalCpu; i++) {
@@ -212,34 +324,61 @@ if (cluster.isPrimary) {
     const Link = req.query.url;
     if (Link) {
       try {
-        if (fs.existsSync(`music/${Link}.mp3`)) {
-          const audio = fs.createReadStream(`music/${Link}.mp3`);
-          const data = fs.statSync(`music/${Link}.mp3`);
+        if (fs.existsSync(`music/${Link}converted.mp3`)) {
+          const audio = fs.createReadStream(`music/${Link}converted.mp3`);
+          const data = fs.statSync(`music/${Link}converted.mp3`);
           res.setHeader("content-type", "audio/mpeg");
           res.setHeader("Accept-Ranges", "bytes");
-
           res.setHeader("content-length", data.size);
+
           audio.pipe(res);
           return;
         }
 
         const Download = StreamAudio(Link, {
-          filter: "videoandaudio",
-          quality: "highestvideo",
+          filter: "audioonly",
+          quality: "highestaudio",
         }).pipe(fs.createWriteStream(`music/${Link}.mp3`));
 
         Download.on("error", () => console.error("error"));
         Download.on("finish", () => {
           console.log(Link);
-          const audio = fs.createReadStream(`music/${Link}.mp3`);
-          const data = fs.statSync(`music/${Link}.mp3`);
+
+          const inputFilePath = `music/${Link}.mp3`;
+
+          const outputFilePath = `music/${Link}converted.mp3`;
+
+          const ffmpegProcess = cp.spawnSync(ffmpeg, [
+            "-i",
+            inputFilePath, // Input file
+            "-c:a",
+            "libmp3lame", // Output audio codec (MP3)
+            "-q:a",
+            "0",
+            "-y", // Overwrite output file if it exists
+            outputFilePath, // Output file
+          ]);
+
+          // Check if the conversion was successful
+          if (ffmpegProcess.status === 0) {
+            console.log("Conversion successful.");
+          } else {
+            console.error(
+              "Error converting file:",
+              ffmpegProcess.stderr.toString()
+            );
+          }
+          fs.unlinkSync(`music/${Link}.mp3`);
+          const audio = fs.createReadStream(`music/${Link}converted.mp3`);
+          const data = fs.statSync(`music/${Link}converted.mp3`);
           res.setHeader("content-type", "audio/mpeg");
           res.setHeader("Accept-Ranges", "bytes");
           res.setHeader("content-length", data.size);
+
           audio.pipe(res);
         });
       } catch (error) {
-        console.log("error");
+        console.log(error.message);
         res.json("error");
       }
     } else {
@@ -251,9 +390,9 @@ if (cluster.isPrimary) {
     const file = req.query.file;
     if (Link) {
       try {
-        if (fs.existsSync(`music/${Link}.mp3`)) {
-          const audio = fs.createReadStream(`music/${Link}.mp3`);
-          const data = fs.statSync(`music/${Link}.mp3`);
+        if (fs.existsSync(`music/${Link}converted.mp3`)) {
+          const audio = fs.createReadStream(`music/${Link}converted.mp3`);
+          const data = fs.statSync(`music/${Link}converted.mp3`);
           res.setHeader("content-type", "audio/mpeg");
           res.setHeader("content-length", data.size);
           res.setHeader("Accept-Ranges", "bytes");
@@ -266,15 +405,41 @@ if (cluster.isPrimary) {
         }
 
         const Download = StreamAudio(Link, {
-          filter: "videoandaudio",
-          quality: "highestvideo",
+          filter: "audioonly",
+          quality: "highestaudio",
         }).pipe(fs.createWriteStream(`music/${Link}.mp3`));
 
         Download.on("error", () => console.error("error"));
         Download.on("finish", () => {
           console.log(Link);
-          const audio = fs.createReadStream(`music/${Link}.mp3`);
-          const data = fs.statSync(`music/${Link}.mp3`);
+          const inputFilePath = `music/${Link}.mp3`;
+
+          const outputFilePath = `music/${Link}converted.mp3`;
+
+          const ffmpegProcess = cp.spawnSync(ffmpeg, [
+            "-i",
+            inputFilePath, // Input file
+            "-c:a",
+            "libmp3lame", // Output audio codec (MP3)
+            "-q:a",
+            "0",
+
+            "-y", // Overwrite output file if it exists
+            outputFilePath, // Output file
+          ]);
+
+          // Check if the conversion was successful
+          if (ffmpegProcess.status === 0) {
+            console.log("Conversion successful.");
+          } else {
+            console.error(
+              "Error converting file:",
+              ffmpegProcess.stderr.toString()
+            );
+          }
+          fs.unlinkSync(`music/${Link}.mp3`);
+          const audio = fs.createReadStream(`music/${Link}converted.mp3`);
+          const data = fs.statSync(`music/${Link}converted.mp3`);
           res.setHeader("content-type", "audio/mpeg");
           res.setHeader("content-length", data.size);
           res.setHeader("Accept-Ranges", "bytes");
@@ -297,3 +462,4 @@ if (cluster.isPrimary) {
     console.log(`http://localhost:${port}`);
   });
 }
+
